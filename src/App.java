14c3f6f34c3473aa203f164929a2912a71c42e3c
desim <manvinderjawanda@gmail.com>
2022-08-07 13:13:14 -0700

    Fixed datatypes

--- src/App.java
+++ src/App.java
@@ -18,8 +18,8 @@ public class App {
         fighterStats[0] = getFighter1Details(fighter1);
         fighterStats[1] = getFighter2Details(fighter2);
         // run an alg to find max score possible out of all fighters -- present user with fighter's demon rating 
-        double fighter1WinPct = 0.0;
-        double fighter2WinPct = 0.0;
+        double fighter1WinPct = (long) 0.0;
+        double fighter2WinPct = (long) 0.0;
         if (fighterStats[0] > fighterStats[1]) {
             fighter1WinPct = (1 - (fighterStats[1])/(fighterStats[0])) * 100 + 50;
             fighter2WinPct = 100 - fighter1WinPct;
@@ -29,6 +29,8 @@ public class App {
         }
         System.out.println(fighter1 + "'s Win Probability: " + Math.round(fighter1WinPct) + "%");
         System.out.println(fighter2 + "'s Win Probability: " + Math.round(fighter2WinPct) + "%");
+        String winner = fighter1WinPct > fighter2WinPct ? fighter1 : fighter2;
+        System.out.println("The winner of this fight is " + winner);
 
         
     }
@@ -40,36 +42,48 @@ public class App {
         for (Object o : fighters) {
             JSONObject person = (JSONObject) o;
             String name = (String) person.get("fighter_name");
-            if (name.equals(fighter1)) {
+            if (name.equalsIgnoreCase(fighter1)) {
                 String td_acc = (String) (person.get("TD_Acc"));
-                td_acc = td_acc.replaceAll("%", "");
                 String reach = (String) (person.get("Reach"));
-                reach = reach.replaceAll("\"", "");
                 String td_def = (String) (person.get("TD_Def"));
-                td_def = td_def.replaceAll("%", "");
                 String str_acc = (String) (person.get("Str_Acc"));
-                str_acc = str_acc.replaceAll("%", "");
                 String str_def = (String) (person.get("Str_Def"));
-                str_def = str_def.replaceAll("%", "");
                 String height = (String) (person.get("Weight"));
-                height= height.replaceAll("\\s+", "").replaceAll("'", "").replaceAll("\"", "");
                 String weight = (String) (person.get("Weight"));
-                weight = weight.replaceAll(" lbs.", "");
-                long sub_avgL = 0;
-                double sub_avgD = 0.0;
-                if (person.get("Sub_Avg") instanceof Long) {
-                    sub_avgL = (long) person.get("Sub_Avg");
-                }
-                if (person.get("Sub_Avg") instanceof Double) {
-                    sub_avgD = (double) person.get("Sub_Avg");
-                }
-                positives1 = (double) person.get("TD_Avg") + (double) person.get("SApM") + (double) person.get("SLpM");
+                Object td_avg = person.get("TD_Avg");
+                Object sapm = person.get("SApM");
+                Object slpm = person.get("SLpM");
+                Object sub_avg = person.get("Sub_Avg");
+                positives1 = longDoubleCaster(positives1, td_avg, sapm, slpm, sub_avg);
                 positives1 += Double.parseDouble(td_acc) + Double.parseDouble(td_def) + Double.parseDouble(str_acc) + Double.parseDouble(str_def) + Double.parseDouble(weight) +
-                    (sub_avgD) + (double) (sub_avgL) + Double.parseDouble(weight) + Double.parseDouble(reach);
+                    + Double.parseDouble(reach) + Double.parseDouble(height);
             }
-            // add reach
         }
         return positives1;
+    }
+
+    private static double longDoubleCaster(double positives, Object td_avg, Object sapm, Object slpm, Object sub_avg) {
+        if (td_avg instanceof Long) {
+            positives += (long) td_avg;
+        } else if (td_avg instanceof Double) {
+            positives += (double) td_avg;
+        }
+        if (sapm instanceof Long) {
+            positives += (long) sapm;
+        } else if (sapm instanceof Double) {
+            positives += (double) sapm;
+        }
+        if (slpm instanceof Long) {
+            positives += (long) slpm;
+        } else if (slpm instanceof Double) {
+            positives += (double) slpm;
+        }
+        if (sub_avg instanceof Long) {
+            positives += (long) sub_avg;
+        } else if (sub_avg instanceof Double) {
+            positives += (double) sub_avg;
+        }
+        return positives;
     }
 
     private static double getFighter2Details(String fighter2) throws FileNotFoundException, IOException, ParseException {
@@ -79,34 +93,23 @@ public class App {
         for (Object o : fighters) {
             JSONObject person = (JSONObject) o;
             String name = (String) person.get("fighter_name");
-            if (name.equals(fighter2)) {
+            if (name.equalsIgnoreCase(fighter2)) {
                 String td_acc = (String) (person.get("TD_Acc"));
-                td_acc = td_acc.replaceAll("%", "");
                 String reach = (String) (person.get("Reach"));
-                reach = reach.replaceAll("\"", "");
                 String td_def = (String) (person.get("TD_Def"));
-                td_def = td_def.replaceAll("%", "");
                 String str_acc = (String) (person.get("Str_Acc"));
-                str_acc = str_acc.replaceAll("%", "");
                 String str_def = (String) (person.get("Str_Def"));
-                str_def = str_def.replaceAll("%", "");
                 String height = (String) (person.get("Weight"));
-                height= height.replaceAll("\\s+", "").replaceAll("'", "").replaceAll("\"", "");
                 String weight = (String) (person.get("Weight"));
-                weight = weight.replaceAll(" lbs.", "");
-                long sub_avgL = 0;
-                double sub_avgD = 0.0;
-                if (person.get("Sub_Avg") instanceof Long) {
-                    sub_avgL = (long) person.get("Sub_Avg");
-                }
-                if (person.get("Sub_Avg") instanceof Double) {
-                    sub_avgD = (double) person.get("Sub_Avg");
-                }
-                positives2 = (double) person.get("TD_Avg") + (double) person.get("SApM") + (double) person.get("SLpM");
+                Object td_avg = person.get("TD_Avg");
+                Object sapm = person.get("SApM");
+                Object slpm = person.get("SLpM");
+                Object sub_avg = person.get("Sub_Avg");
+                positives2 = longDoubleCaster(positives2, td_avg, sapm, slpm, sub_avg);
                 positives2 += Double.parseDouble(td_acc) + Double.parseDouble(td_def) + Double.parseDouble(str_acc) + Double.parseDouble(str_def) + Double.parseDouble(weight) +
-                    (sub_avgD) + (double) (sub_avgL) + Double.parseDouble(weight) + Double.parseDouble(reach);
+                     + Double.parseDouble(reach) + Double.parseDouble(height);
+            }
         }
-    }
     return positives2;
     }
 }
